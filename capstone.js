313b8a4a495176ac1f09ff7cdc4a693d8cdaf117
Cristopher Perez <c.perez86@yahoo.com>
2023-11-24 17:56:11 -0800

    doubled checked image sources

--- capstone.js
+++ capstone.js
@@ -1,11 +1,3 @@
-// function toggleMenu() {
-//     var navbarMobile = document.getElementById("navbar-mobile");
-//     if (navbarMobile.style.display === "block") {
-//         navbarMobile.style.display = "none";
-//     } else {
-//         navbarMobile.style.display = "block";
-//     }
-// }
 
 function toggleMenu() {
     var navbarMobile = document.getElementById("navbar-mobile");
